058010032650103136706b0ed69a2a021a15de9a
Mauro Gauto <elvis.gauto1999@gmail.com>
2021-02-03 00:45:07 -0300

    add forms and get http

--- frontend/src/app/components/employee/employee.component.ts
+++ frontend/src/app/components/employee/employee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+
+import { Employee } from '../../models/employee.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent implements OnInit {
+  employeeForm: FormGroup[] = [];
+  employeeArr: Employee[] = [];
 
-  constructor() { }
+  constructor(
+    private employeeService: EmployeeService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.getEmployees();
+    this.generateEmployeeForm();
+  }
+
+  getEmployees(): void {
+    this.employeeService.getEmployees$().subscribe(data => {
+      this.employeeArr = data;
+    });
+  }
+
+  generateEmployeeForm(): void {
+    // this.employeeForm  = this.fb.group({
+    //   name: '',
+    //   position: '',
+    //   office: '',
+    //   salary: ''
+    // });
   }
 
 }
---
+++ frontend/src/app/services/employee.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  URL_API = 'http://localhost:9000/api/employees';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getEmployees$(): Observable<any> {
+    return this.http.get(this.URL_API);
+  }
+
+  submitEmployees$(employee: Employee): any {
+    this.http.post(this.URL_API, employee);
+  }
+}
